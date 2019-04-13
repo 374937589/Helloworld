1c8f41ad00989e5fdb1762f94a9969234473b68a
374937589 <374937589@qq.com>
2019-04-13 10:47:54 +0800

    “我从idea环境中推送该项目"

---
+++ src/com/bdqn/Test3.java
@@ -0,0 +1,10 @@
+package com.bdqn;
+
+/**
+ * Created by Administrator on 2019/4/13.
+ */
+public class Test3 {
+    public static void mian(String []args){
+        System.out.println("hello song hao wen ");
+    }
+}
