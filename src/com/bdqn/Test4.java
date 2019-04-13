c630432608f99b755ca7d0fe9e01d0e0ebebe276
374937589 <374937589@qq.com>
2019-04-13 10:52:29 +0800

    “我从idea环境中推送该项目"

---
+++ src/com/bdqn/Test4.java
@@ -0,0 +1,10 @@
+package com.bdqn;
+
+/**
+ * Created by Administrator on 2019/4/13.
+ */
+public class Test4 {
+    public static void main(String [] args){
+        System.out.println("sun zi sun pei guo ");
+    }
+}
